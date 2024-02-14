d1d0d7a1c3ce0fcbe96695e6bab785be3aef3c1b
Damián Liñeiro <delineiro@gmail.com>
2024-02-14 18:39:32 -0300

    Cards destination

---
+++ app/components/CardDestinations.js
@@ -0,0 +1,30 @@
+export default function CardDestinations({ imgUrl, locationName, state, country, price }) {
+  return (
+    <main>
+      <div className="w-full flex flex-row justify-start items-center gap-5 my-8">
+        <div style={{ "--image-url": `url(${imgUrl})` }} className="aspect-video h-72 bg-center bg-[image:var(--image-url)] bg-cover rounded-2xl flex flex-row justify-start items-end p-2">
+          <div className="flex flex-row justify-between items-center backdrop-blur-xl px-4 py-2 bg-black/20 border border-white/30 text-white rounded-md w-full gap-5">
+            <div className="flex flex-col justify-start items-start text-lg">
+              <div>{locationName}</div>
+              <div className="flex flex-row justify-start items-center gap-2 text-xs">
+                <div>
+                  <i className="ri-map-pin-2-fill text-sm"></i>
+                </div>
+                <span>{state}, {country}</span>
+              </div>
+            </div>
+            <div className="flex flex-col justify-start items-start text-2xl font-medium">
+              <div>${price}</div>
+              <div className="flex flex-row justify-start items-center gap-2 text-xs">
+                <div>
+                  <i className="ri-user-smile-line text-sm"></i>
+                </div>
+                <span>/person</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
--- app/components/CardPlaces.js
+++ app/components/CardPlaces.js
@@ -1,3 +1,7 @@
+"use client";
+
+import CardDestinations from "./CardDestinations";
+
 export default function CardPlaces() {
   return (
     <main className="w-full px-8 py-8 container">
@@ -6,13 +10,36 @@ export default function CardPlaces() {
           Dive Into The Beauty <br /> Of The World
         </h1>
         <div className="flex flex-row gap-6">
-          <button className=" rounded-full w-10 h-10 hover:bg-sky-500 hover:text-white text-black bg-white text-xl transition duration-300 ease-in-out">
+          <button className="rounded-full w-10 h-10 hover:bg-sky-500 hover:text-white text-black bg-white text-xl transition duration-300 ease-in-out flex items-center justify-center">
             <i className="ri-arrow-left-s-line"></i>
           </button>
-          <button className="rounded-full w-10 h-10 hover:bg-sky-500 hover:text-white text-black bg-white text-xl transition duration-300 ease-in-out">
+          <button className="rounded-full w-10 h-10 hover:bg-sky-500 hover:text-white text-black bg-white text-xl transition duration-300 ease-in-out flex items-center justify-center">
             <i className="ri-arrow-right-s-line"></i>
           </button>
         </div>
+      </div>
+      <div className="flex flex-row justify-start items-center gap-5">
+        <CardDestinations
+          imgUrl={"https://cdn1.matadornetwork.com/blogs/1/2022/07/nusa-penida-bali-islands-1200x900.jpg"}
+          locationName={"Bali Island"}
+          state={"Bali"}
+          country={"Indonesia"}
+          price={224}
+        />
+        <CardDestinations
+          imgUrl={"https://dsvsbigncb06y.cloudfront.net/site/diving/indonesia/indonesiagiliislandsxladobestock286429634.jpg"}
+          locationName={"Gili Trawangan"}
+          state={"Lombok"}
+          country={"NTT"}
+          price={224}
+        />
+        <CardDestinations
+          imgUrl={"https://mejorepocapara.net/wp-content/uploads/2019/01/mejor-epoca-para-viajar-a-gran-canon-del-colorado.jpg"}
+          locationName={"Grand Canyon"}
+          state={"Arizona"}
+          country={"United State"}
+          price={386}
+        />
       </div>
     </main>
   );
